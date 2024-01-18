d1a3dd98c2fc007f0ce9c3fc85fdfbbb27ae4220
Ben Schreiber <bpschreiber2003@gmail.com>
2024-01-18 15:33:28 -0800

    jsdoc example

---
+++ index.js
@@ -0,0 +1,80 @@
+// https://jsdoc.app
+
+
+/**
+ * Adds two numbers
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function add(a, b) {
+    return a + b
+}
+
+/**
+ * Adds two numbers
+ * @param {number} a
+ * @param {number} b
+ * @example
+ * // returns 3
+ * add(1, 2)
+ * @returns {number}
+ */
+const add = (a, b) => a + b
+
+/**
+ * Enum for tri-state values.
+ * @readonly
+ * @enum {number}
+ */
+var triState = {
+    /** The true value */
+    TRUE: 1,
+    FALSE: -1,
+
+    /** @type {boolean} */
+    MAYBE: true
+};
+
+/** Represents a cartesian point */
+class Point {
+    /**
+     * Create a point.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     */
+    constructor(x, y) {
+        // ...
+    }
+
+    /**
+     * @return {number} The x value.
+     */
+    getX() {
+        // ...
+    }
+
+    /**
+     * @return {number} The y value.
+     */
+    getY() {
+        // ...
+    }
+
+    /**
+     * Convert a string containing two comma-separated numbers into a point.
+     * @param {string} str - The string containing two comma-separated numbers.
+     * @return {Point}
+     */
+    static fromString(str) {
+        // ...
+    }
+}
+
+/**
+ * Class representing a dot.
+ * @extends Point
+ */
+class Dot extends Point {
+    // ...
+}
